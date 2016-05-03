9ba18c8b6d7dfbb375f8b30d0fdbb92a283f4f18
Love Bisaria <love.bisaria@ticketmaster.com>
2016-05-03 11:53:27 -0700

    adding the directory structure

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,20 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by lovebisaria on 03/05/16.
  */
 @Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {"com.ticketmaster.bot"})
 @PropertySource({"classpath:application.properties" })
 public class Application {
